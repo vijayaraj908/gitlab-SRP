fe7709754b63b26b4b8b85e11f68f17cfb4d2fed
vijayaraj908 <vijayaraj.innovate@gmail.com>
2026-06-20 12:33:23 +0000

    Updated java,xml file

--- DemoApplication.java
+++ DemoApplication.java
@@ -2,14 +2,14 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+// We exclude DataSourceAutoConfiguration only if you don't have your DB connection set up yet.
+// If you want to use your SQL Server, you can remove the (exclude = ...) part.
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
 }
-
