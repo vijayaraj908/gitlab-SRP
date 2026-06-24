30219512e672c1affc82e517e2c019492b2ce378
vijayaraj908 <vijayaraj.innovate@gmail.com>
2026-06-24 04:17:26 +0000

    Updated Filter feature java files

--- src/main/java/StudentRepository.java
+++ src/main/java/StudentRepository.java
@@ -2,7 +2,10 @@ package com.example.demo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
+    // This allows you to find students where the 'course' field matches any in the list
+    List<Student> findByCourseIn(List<String> courses);
 }
